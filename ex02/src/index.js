6d7a4f912c202368e2e839575d44596b7fafe447
Rima-Mec <dmerima08@gmail.com>
2022-03-31 00:29:32 +0200

    Adding index.js file

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+    // Only change this code below this line
+    // This is a single line comment
+    /* This is a multiline comment;
+    This is a multiline comment;
+    */
+    // Only change this code above this line
+}
+
+console.log(main());
+
+module.exports = main;
