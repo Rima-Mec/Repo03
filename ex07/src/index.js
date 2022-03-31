ff5bd24fc8040f79e350c0f51226877f3d2809ee
Rima-Mec <dmerima08@gmail.com>
2022-03-31 03:09:28 +0200

    Adding index.js file

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main () {
+    // Only change code belowe this line
+    // Variable declarations
+    var studlyCapeVar;
+    var properCamelCase;
+    var titleCaseOver;
+    // Variable assignments
+    studlyCapeVar = 10;
+    properCamelCase = "A string ";
+    titleCaseOver = 9000;
+    // Only change code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+
+module.exports = main
