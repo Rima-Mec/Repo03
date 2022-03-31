33ac76a435b538a5ca8031d415a23978943a98c8
Rima-Mec <dmerima08@gmail.com>
2022-03-31 05:01:34 +0200

    Adding index.js file

---
+++ ex08/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwo) {
+    // Only change code belowe this line
+    var sum;
+    var numOne;
+    var numTwo;
+    numOne = (15, 5, 150);
+    numTwo = (8, 28, 425);
+    sum = numOne + numTwo;  
+    sum = 5 + 8;
+    sum = 15 + 28;
+    sum = 150 + 425;
+    // Only change code above this line
+    return sum;
+}
+console.log(main(numOne, numTwo)); // change this line
+console.log(main(15, 8)); // assigned 23
+console.log(main(5, 28)); // assigned 33
+console.log(main(150, 425)); // assigned 575
+
+module.exports = main;
