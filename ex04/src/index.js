cceca3903867d1baeef0b966eb9eeafecddb73d7
Rima-Mec <dmerima08@gmail.com>
2022-04-03 00:31:31 +0200

    Adding index.js file

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,15 +1,13 @@
 function main () {
     // Only change code below this line
     var x;
-    var y;
     x = 10;
-    y = 10;
+    var y;
+    x = y;
     // Only change this code above this line
     return [x, y];
 }
 
-
-        console.log(main());
-    
-        module.exports = main
-        
+ console.log(main());
+ 
+ module.exports = main;
